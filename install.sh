d374cb17c1b11de8e0aca7b6267f822ec2c8e930
Gabe Tao <93845863+t-ebag@users.noreply.github.com>
2022-09-27 14:00:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -176,7 +176,7 @@ _task "reload system"
 printf "${OVERWRITE}${LGREEN} [✓]  ${LGREEN}${TASK}\n"
 
 # remove conduro.log
-rm conduro.log
+# rm conduro.log #I changed this
 
 # reboot
 printf "\n${YELLOW} Do you want to reboot [Y/n]? ${RESTORE}"
